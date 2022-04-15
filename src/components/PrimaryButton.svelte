8afdf5c6fd2a589fb682e3840095b8b73988f62d
Joaquín R. Montes <joaco.rm512@gmail.com>
2022-04-15 16:41:07 -0300

    [JC-2]: Created UI components, modularized app
    Separated the app in isolated components and removed most global styles.

---
+++ src/components/PrimaryButton.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let shadow = "none";
+</script>
+
+<button class="btn" style:box-shadow={shadow}>
+  <slot />
+</button>
+
+<style>
+  .btn {
+    color: white;
+    background-color: dodgerblue;
+    border: none;
+    line-height: 48px;
+    border-radius: 4px;
+    font-size: 18px;
+    font-weight: 500;
+    cursor: pointer;
+    padding: 0 16px;
+  }
+</style>
