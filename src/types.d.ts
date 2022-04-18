22fada627b6935c8b36a8c2902b1616a7598a6f1
JustKahdri <joaco.rm512@gmail.com>
2022-04-18 19:22:37 -0300

    [JC-1]: Updated types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,5 @@
+import type { Unsubscriber, Subscriber, Invalidator, Writable } from "svelte/store";
+
 declare global {
   interface ProductT {
     id: string;
@@ -8,6 +10,20 @@ declare global {
     title: string;
     description: string;
     image: string;
+  }
+
+  type MouseEventHandler = (event: MouseEvent) => void;
+
+  type CartWritable = Writable<Map<string, CartProduct>>
+
+  interface Cart {
+    subscribe: (
+      this: void,
+      run: Subscriber<Map<string, CartProduct>>,
+      invalidate?: Invalidator<Map<string, CartProduct>> | undefined)
+      => Unsubscriber;
+    addProduct: (product: ProductT) => void;
+    removeProduct: (product: ProductT) => void;
   }
 }
 
