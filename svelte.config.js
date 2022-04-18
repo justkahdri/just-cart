1daaa9043c21efc3331f4e0499d4f1c65e35287e
JustKahdri <joaco.rm512@gmail.com>
2022-04-18 19:22:09 -0300

    [JC-1]: Added path aliases

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,23 @@
 import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
+import { resolve } from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+    vite: {
+			resolve: {
+				alias: {
+          "@components": resolve("./src/components"),
+          "@model": resolve("./src/model"),
+          "@utils": resolve("./src/utils"),
+          "@cart": resolve("./src/stores/cart")
+				}
+			}
+    }
 	}
 };
 
